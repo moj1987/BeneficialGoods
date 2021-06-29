ef01220fe276bb0718fd2781e0e6854ce220b8bc
Nikunj Prajapati <nikunjd.np@gmail.com>
2021-06-29 10:20:48 -0400

    Network interface and DataModel created.

---
+++ BeneficialGoods/BeneficialGoods/Networking/NetworkService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficialGoods.Networking
+{
+    interface NetworkService
+    {
+        DataModel GetData();
+    }
+}
---
+++ BeneficialGoods/BeneficialGoods/Networking/NetworkServiceImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficialGoods.Networking
+{
+    class NetworkServiceImpl : NetworkService
+    {
+        public DataModel GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
