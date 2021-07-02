9988f9aff9e8028d1e74733dd9a8bb3a7f3f2f4f
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-07-01 21:36:25 -0400

    Implement ReportDataModel

--- BeneficialGoods/BeneficialGoods/Model/ReportDataModel.cs
+++ BeneficialGoods/BeneficialGoods/Model/ReportDataModel.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace BeneficialGoods.Model
 {
-    class ReportDataModel
+    internal class ReportDataModel
     {
+        public ReportDataModel(long productId, string productName, decimal contractPrice, decimal quantitySold)
+        {
+        }
+
+        public long ProductId
+        {
+            get; set;
+        }
+
+        public string ProductName
+        {
+            get;
+            set;
+        }
+
+        private decimal _contractPrice;
+
+        public decimal ContractPrice
+        {
+            get { return _contractPrice; }
+            set { _contractPrice = value; }
+        }
+
+        private int QuantitySold;
+
+        public int _quantitySold
+        {
+            get { return _quantitySold; }
+            set { _quantitySold = value; }
+        }
+
+        public decimal calculateNetPrice(decimal fees)
+        {
+            decimal netPrice = ContractPrice - fees;
+            return netPrice;
+        }
+
+        public decimal calculatePayoutPerItem(decimal netPrice)
+        {
+            decimal payoutPerItem = netPrice * QuantitySold;
+            return payoutPerItem;
+        }
     }
 }
