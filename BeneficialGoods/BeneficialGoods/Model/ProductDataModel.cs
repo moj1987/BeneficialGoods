fb2eeead82a0f26a3901144eef50cf4c80d6be2e
Mojtaba Abbasszadeh <mojtaba.abbasszadeh@gmail.com>
2021-07-20 14:03:06 -0400

    Add ProductDataModel

---
+++ BeneficialGoods/BeneficialGoods/Model/ProductDataModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficialGoods.Model
+{
+    internal class ProductDataModel
+    {
+        public ProductDataModel(long? productId, string vendor, string tags)
+        {
+            ProductId = productId;
+            Vendor = vendor;
+            Tags = tags;
+        }
+
+        public long? ProductId
+        {
+            get; set;
+        }
+
+        public string Vendor
+        {
+            get;
+            set;
+        }
+
+        public string Tags
+        {
+            get;
+            set;
+        }
+    }
+}
