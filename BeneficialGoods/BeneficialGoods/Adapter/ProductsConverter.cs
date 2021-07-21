209818684d156b2acde5cffc814a7ce4a25472b9
Mark Dracopoulos <mark.dracopoulos@gmail.com>
2021-07-21 13:56:39 -0400

    Add json Product Converter

---
+++ BeneficialGoods/BeneficialGoods/Adapter/ProductsConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Data;
+using Newtonsoft.Json.Linq;
+using BeneficialGoods.Model;
+using BeneficialGoods.Utilities;
+
+namespace BeneficialGoods.Adapter
+{
+    class ProductsConverter
+    {
+        
+        public List<ProductDataModel> GetProduct()
+        {
+            ProductItemsModel product = JsonSerializer.Deserialize<ProductItemsModel>(SampleProductString.GetSampleProducts());
+            var allProducts = product.products.ToList();
+            List<ProductDataModel> reportProducts = new List<ProductDataModel>();
+            foreach (ProductDataModel p in allProducts)
+            {
+                ProductDataModel products = new ProductDataModel(p.ProductId, p.Vendor, p.Tags);
+                reportProducts.Add(products);
+            }
+            return reportProducts;
+        }
+
+    }
+}
