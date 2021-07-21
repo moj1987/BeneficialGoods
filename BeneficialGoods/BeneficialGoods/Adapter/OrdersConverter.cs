65ae6f0d93df8b943c3e2de139450cd15b61534b
Mark Dracopoulos <mark.dracopoulos@gmail.com>
2021-07-21 13:56:16 -0400

    Add json Order Converter

---
+++ BeneficialGoods/BeneficialGoods/Adapter/OrdersConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Data;
+using Newtonsoft.Json.Linq;
+using BeneficialGoods.Model;
+using BeneficialGoods.Utilities;
+
+namespace BeneficialGoods.Adapter
+{
+    internal class OrdersConverter
+    {  
+        public List<ReportDataModel> GetOrder()
+        {
+            OrderItemsListModel order = JsonSerializer.Deserialize<OrderItemsListModel>(SampleReportString.GetSampleData());
+            var allOrders = order.orders.ToList();
+            List<ReportDataModel> reportOrders = new List<ReportDataModel>();
+            foreach (AllItemsModel a in allOrders)
+            {
+                foreach (ReportDataModel l in a.line_items)
+                {
+                    ReportDataModel reportDataModel = new ReportDataModel(l.ProductId, l.ProductName, l.ContractPrice, l.QuantitySold);
+                    reportOrders.Add(reportDataModel);
+                    
+                }
+            }
+            return reportOrders;
+        }
+    }
+}
