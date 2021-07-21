fd3fe36c374b1d478a5324f4a6093488dcf9c607
Mark Dracopoulos <mark.dracopoulos@gmail.com>
2021-07-21 13:55:22 -0400

    Add Product and Order Model Classes

---
+++ BeneficialGoods/BeneficialGoods/Model/AllItemsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficialGoods.Model
+{
+    class AllItemsModel
+    {
+        public string created_at { get; set; }
+        public string financial_status { get; set; }
+        public IList<ReportDataModel> line_items { get; set; }
+    }
+}
---
+++ BeneficialGoods/BeneficialGoods/Model/ProductItemsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeneficialGoods.Model
+{
+    class ProductItemsModel
+    {
+        public IList<ProductDataModel> products { get; set; }
+    }
+}
